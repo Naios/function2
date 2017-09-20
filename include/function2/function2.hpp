35a5f4f4891a3c00fdeba46d7699788de8874302
Denis Blank <denis.blank@outlook.com>
2017-09-20 14:04:57 +0200

    Remove utf bom from some files * Caused by a bug in the MSVC editorconfig support

--- include/function2/function2.hpp
+++ include/function2/function2.hpp
@@ -1,4 +1,4 @@
-﻿
+
 //  Copyright 2015-2017 Denis Blank <denis.blank at outlook dot com>
 //     Distributed under the Boost Software License, Version 1.0
 //       (See accompanying file LICENSE_1_0.txt or copy at
