63c16fc5df97ff74c65f285daa226ba06cf62b66
Denis Blank <denis.blank@outlook.com>
2018-04-13 14:57:07 +0200

    Update the copyright to 2018

--- test/standard-compliant-test.cpp
+++ test/standard-compliant-test.cpp
@@ -1,5 +1,5 @@
 
-//  Copyright 2015-2017 Denis Blank <denis.blank at outlook dot com>
+//  Copyright 2015-2018 Denis Blank <denis.blank at outlook dot com>
 //     Distributed under the Boost Software License, Version 1.0
 //       (See accompanying file LICENSE_1_0.txt or copy at
 //             http://www.boost.org/LICENSE_1_0.txt)
