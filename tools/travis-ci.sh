235ad25af3eb27952bf5f6a36f8fbd49d27ee951
Denis Blank <denis.blank@outlook.com>
2019-09-24 11:04:27 +0200

    Check the release build in the CI on linux

--- tools/travis-ci.sh
+++ tools/travis-ci.sh
@@ -11,7 +11,7 @@ export PATH=${DEPS_DIR}/cmake/bin:${PATH}
 cmake --version
 
 # Function for creating a new 'build' directory
-function renew_build {
+function renew_build() {
   echo "Renew build directory..."
   cd $TRAVIS_BUILD_DIR
 
@@ -23,7 +23,7 @@ function renew_build {
   # Configure the project and build it
   cmake -GNinja -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -Werror" \
         -DFU2_WITH_NO_EXCEPTIONS=$NO_EXCEPTIONS -DFU2_WITH_NO_DEATH_TESTS=ON \
-        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug ..
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=$1 ..
 }
 
 if [[ $CXX == *"clang"* ]]; then
@@ -35,7 +35,7 @@ if [[ $CXX == *"clang"* ]]; then
   # - ASan (LSan):
   echo "Building with address sanitizer..."
   CMAKE_CXX_FLAGS="-fsanitize=address -fno-omit-frame-pointer"
-  renew_build
+  renew_build Debug
 
   ninja -j2
   ctest --verbose
@@ -43,14 +43,21 @@ if [[ $CXX == *"clang"* ]]; then
   # - UBSan:
   echo "Building with undefined behaviour sanitizer..."
   CMAKE_CXX_FLAGS="-fsanitize=undefined -fno-omit-frame-pointer"
-  renew_build
+  renew_build Debug
 
   ninja -j2
   ctest --verbose
 else
   # Build an run the tests suite with valgrind
-  renew_build
+  echo "Building for valgrind..."
+  renew_build Debug
 
   ninja -j2
   valgrind --error-exitcode=1 --leak-check=full --show-reachable=yes ctest --verbose
 fi
+
+# Also check the release builds of all toolchains
+echo "Building in Release mode..."
+renew_build Release
+ninja -j2
+ctest --verbose
