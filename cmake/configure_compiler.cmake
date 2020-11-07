fb903bbf00109c294f2a0dc742d968bf52be3883
Richard Hodges <hodges.r@gmail.com>
2020-11-07 20:18:28 +0100

    Allow configuring for AppleClang

--- cmake/configure_compiler.cmake
+++ cmake/configure_compiler.cmake
@@ -1,11 +1,11 @@
 # Select the compiler specific cmake file
 set(MSVC_ID "MSVC")
-if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+if (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
   include(${CMAKE_SOURCE_DIR}/cmake/compiler/clang.cmake)
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
   include(${CMAKE_SOURCE_DIR}/cmake/compiler/gcc.cmake)
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL ${MSVC_ID})
   include(${CMAKE_SOURCE_DIR}/cmake/compiler/msvc.cmake)
 else()
-  message(FATAL_ERROR "Unknown compiler!")
+  message(FATAL_ERROR "Unknown compiler: ${CMAKE_CXX_COMPILER_ID}")
 endif()
