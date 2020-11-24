c8eb35a8b4f6292905ae174838db9ff589a2da62
maierlars <lars@arangodb.com>
2020-11-24 16:54:49 +0100

    Applied clang-format.

--- include/function2/function2.hpp
+++ include/function2/function2.hpp
@@ -279,9 +279,9 @@ template <bool RequiresNoexcept, typename T, typename Args>
 struct is_noexcept_correct : std::true_type {};
 template <typename T, typename... Args>
 struct is_noexcept_correct<true, T, identity<Args...>>
-    : std::integral_constant<bool, noexcept((invoke)(std::declval<T>(),
-                                                   std::declval<Args>()...))> {
-};
+    : std::integral_constant<bool,
+                             noexcept((invoke)(std::declval<T>(),
+                                               std::declval<Args>()...))> {};
 } // end namespace invocation
 
 namespace overloading {
