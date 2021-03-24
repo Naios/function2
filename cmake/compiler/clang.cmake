c14e045eebea8b748b72f9d531c2cb15296af7e4
Gregor Jasny <gjasny@googlemail.com>
2021-03-24 08:23:46 +0100

    feat: enable Clang documentation warnings

--- cmake/compiler/clang.cmake
+++ cmake/compiler/clang.cmake
@@ -1,5 +1,5 @@
 # Enable full warnings
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic -Wextra")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wdocumentation -pedantic -Wextra")
 
 if (FU2_WITH_NO_EXCEPTIONS)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-exceptions")
